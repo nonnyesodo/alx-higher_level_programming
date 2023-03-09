3fa97f33fda8a4e62315311b8899fd1b6b67028f
esodo john <nonnyesodo@gmail.com>
2023-03-09 12:24:26 -0800

    0x02. python - import & modules

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+if __name__ == '__main__':
+    import hidden_4
+    for index in dir(hidden_4):
+        if index in dir(hidden_4):
+            if index[:2] != '__':
+                print('{}'.format(index))
